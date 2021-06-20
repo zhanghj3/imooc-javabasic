1fc0c1815d1c7bd5967888c08732766aea372e03
zhanghj3 <1064356458@qq.com>
2021-06-20 23:40:42 +0800

    java编译，运行，查看字节码命令

---
+++ src/com/imooc/javabasic/bytecode/ByteCodeSample.java
@@ -0,0 +1,14 @@
+package com.imooc.javabasic.bytecode;
+
+public class ByteCodeSample {
+    // 1.编译javac
+    // 2.运行java
+    // 3.查看字节码javap
+    public static void main(String[] args) {
+        int i=1,j=5;
+        i++;
+        ++j;
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
